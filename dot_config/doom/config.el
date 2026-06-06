b5b4c010bff9b856889983c12dc81b707029148a
Eren Kaplan <erenkaplansec@gmail.com>
2026-06-06 22:45:18 +0300

    org-mode specific inclusions and exclusions

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -1,93 +1,91 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;;; Identity
+(setq user-full-name  "Eren Kaplan"
+      user-mail-address "erenkaplansec@gmail.com")
 
+;;; Appearance
+(setq doom-font          (font-spec :family "SF Mono" :size 13)
+      doom-variable-pitch-font (font-spec :family "SF Pro Text" :size 13)
+      doom-theme         'doom-tokyo-night
+      display-line-numbers-type 'relative)
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
- (setq user-full-name "Eren Kaplan"
-       user-mail-address "erenkaplan@tutanota.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tokyo-night)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+;;; Org directories
 (setq org-directory "~/notes/org/")
-(setq org-roam-directory (file-truename "~/notes/roam/"))
-(setq org-roam-file-extensions '("org"))
-
-(org-roam-db-autosync-mode)
-
-(use-package! org-roam-ui
-  :after org-roam
-  :hook (org-roam-mode . org-roam-ui-mode)
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
-
-(use-package vterm
-  :ensure t
-  :commands vterm)
 
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;;; Org-mode
+(after! org
+  ;; Agenda files
+  (setq org-agenda-files
+        '("~/notes/org/inbox.org"
+          "~/notes/org/agenda.org"
+          "~/notes/org/projects.org"))
+
+  ;; Todo keywords
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+
+  (setq org-todo-keyword-faces
+        '(("TODO"      . (:foreground "#e0e0e0" :weight bold))
+          ("NEXT"      . (:foreground "#7a90b8" :weight bold))
+          ("WAITING"   . (:foreground "#b8a87a" :weight bold))
+          ("DONE"      . (:foreground "#505050" :weight bold))
+          ("CANCELLED" . (:foreground "#3a3a3a" :weight bold))))
+
+  ;; Capture templates
+  (setq org-capture-templates
+        '(("i" "Inbox" entry
+           (file "~/notes/org/inbox.org")
+           "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n")
+
+          ("n" "Note" entry
+           (file+headline "~/notes/org/notes.org" "Notes")
+           "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n")
+
+          ("j" "Journal" entry
+           (file+olp+datetree "~/notes/org/journal.org")
+           "* %?\n%U\n")
+
+          ("a" "Agenda" entry
+           (file+headline "~/notes/org/agenda.org" "Tasks")
+           "* TODO %?\nSCHEDULED: %^t\n")))
+
+  ;; Refile targets
+  (setq org-refile-targets
+        '(("~/notes/org/agenda.org"   :maxlevel . 2)
+          ("~/notes/org/projects.org" :maxlevel . 2)
+          ("~/notes/org/notes.org"    :maxlevel . 1)))
+
+  ;; Appearance
+  (setq org-hide-leading-stars     t
+        org-ellipsis               " ▾"
+        org-startup-indented       t
+        org-startup-folded         'content
+        org-pretty-entities        t
+        org-hide-emphasis-markers  t)
+
+  ;; Deadline / scheduling
+  (setq org-deadline-warning-days    7
+        org-agenda-span              'week
+        org-agenda-start-on-weekday  1) ; Monday
+
+  ;; Archive
+  (setq org-archive-location "~/notes/org/archive.org::* Archive"))
+
+;; (setq org-roam-directory (file-truename "~/notes/roam/"))
+;; (setq org-roam-file-extensions '("org"))
+;; (org-roam-db-autosync-mode)
+
+;;; Keymaps
+(map! :leader
+      ;; Org capture
+      :desc "Org capture"      "X"   #'org-capture
+
+      (:prefix ("o" . "org")
+       :desc "Agenda"          "a"   #'org-agenda
+       :desc "Capture"         "c"   #'org-capture
+       :desc "Inbox"           "i"   (cmd! (find-file "~/notes/org/inbox.org"))
+       :desc "Notes"           "n"   (cmd! (find-file "~/notes/org/notes.org"))
+       :desc "Journal"         "j"   (cmd! (find-file "~/notes/org/journal.org"))
+       :desc "Projects"        "p"   (cmd! (find-file "~/notes/org/projects.org"))))
